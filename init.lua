056038c5e0d34b4b103f2b1da94287e07a4a782b
Benjamin Curis-Friedman <bencf99@gmail.com>
2026-05-06 16:36:03 -0400

    catppuccin colorscheme
    deactivated tokyonight-night -> installed & activated catppuccin-mocha

--- init.lua
+++ init.lua
@@ -836,7 +836,19 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
+    end,
+  },
+
+  {
+    'catppuccin/nvim',
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('catppuccin').setup {}
+
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
 
